31f512a6680bde35aae6c6ab16217cd04eadd09b
Tom Perkins <85560933+twperkins@users.noreply.github.com>
2021-09-16 14:35:13 +0100

    updated url

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -3,7 +3,7 @@ class MuseumsController < ApplicationController
     if params[:longitude].blank? || params[:latitude].blank?
       @museums = 'enter coordinates'
     else
-      url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?proximity=#{params[:latitude]},#{params[:longitude]}limit=5&types=poi&access_token=pk.eyJ1IjoidHdwZXJraW5zIiwiYSI6ImNrdG1zbXZ4ejB0cTYydW1qY2UzaGZmbG4ifQ.68F9nxoAw-uieONOJd8LHg"
+      url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?proximity=#{params[:latitude]},#{params[:longitude]}&limit=5&types=poi&access_token=pk.eyJ1IjoidHdwZXJraW5zIiwiYSI6ImNrdG1zbXZ4ejB0cTYydW1qY2UzaGZmbG4ifQ.68F9nxoAw-uieONOJd8LHg"
       @museums = JSON.parse(URI.open(url).read)["features"]
     end
   end
